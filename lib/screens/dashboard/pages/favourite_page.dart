e7526034a3cc85ed2c2b3c994d61f054106caee7
fawad kaleem <fwdkaleem@gmail.com>
2023-11-18 05:22:12 +0500

    Favourite Screen

--- lib/screens/dashboard/pages/favourite_page.dart
+++ lib/screens/dashboard/pages/favourite_page.dart
@@ -1,106 +1,54 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:exploreden/screens/detail/favourite_detail.dart';
-import 'package:exploreden/screens/detail/location_detail.dart';
-import 'package:exploreden/utils/colors.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FavouritePage extends StatefulWidget {
-  const FavouritePage({super.key});
-
   @override
-  State<FavouritePage> createState() => _FavouritePageState();
+  _FavouritePageState createState() => _FavouritePageState();
 }
 
 class _FavouritePageState extends State<FavouritePage> {
+  Future<Map<String, String>> getLocationData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String photoPath = prefs.getString('photoUrl') ?? '';
+    String name = prefs.getString('name') ?? '';
+    String address = prefs.getString('address') ?? '';
+
+    return {'Photo Path': photoPath, 'Name': name, 'Address': address};
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            automaticallyImplyLeading: false,
-            centerTitle: true,
-            title: const Text(
-              "Saved Trips",
-            )),
-        body: SizedBox(
-          height: 225,
-          child: StreamBuilder(
-              stream: FirebaseFirestore.instance
-                  .collection("favourite")
-                  .where("uid",
-                      isEqualTo: FirebaseAuth.instance.currentUser!.uid)
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
+      appBar: AppBar(
+        title: Text('Saved Data'),
+      ),
+      body: FutureBuilder<Map<String, String>>(
+        future: getLocationData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No saved data.'));
+          } else {
+            return SizedBox(
+              height: 300,
+              child: ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  String key = snapshot.data!.keys.elementAt(index);
+                  String value = snapshot.data![key]!;
+
+                  return ListTile(
+                    title: Text('$key: $value'),
                   );
-                }
-                if (snapshot.data!.docs.isEmpty) {
-                  return const Center(
-                    child: Text(
-                      "No Trips saved  yet",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  );
-                }
-                return ListView.builder(
-                    itemCount: snapshot.data!.docs.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return StreamBuilder<Object>(
-                          stream: FirebaseFirestore.instance
-                              .collection("favourite")
-                              .where("uid",
-                                  isEqualTo:
-                                      FirebaseAuth.instance.currentUser!.uid)
-                              .snapshots(),
-                          builder:
-                              (BuildContext context, AsyncSnapshot snapshot) {
-                            if (!snapshot.hasData) {
-                              return const Center(
-                                child: CircularProgressIndicator(),
-                              );
-                            }
-                            final List<DocumentSnapshot> documents =
-                                snapshot.data!.docs;
-                            final Map<String, dynamic> data =
-                                documents[index].data() as Map<String, dynamic>;
-                            return Padding(
-                                padding: const EdgeInsets.all(4.0),
-                                child: Card(
-                                  child: ListTile(
-                                    onTap: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (builder) =>
-                                                  FavourteDetail(
-                                                    uuid: data['uuid'],
-                                                    name: data['Location Name'],
-                                                    description: data[
-                                                        'Location Description'],
-                                                    address: data[
-                                                        'location Address'],
-                                                  )));
-                                    },
-                                    title: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text("Location Name"),
-                                        Text(data['Location Name'])
-                                      ],
-                                    ),
-                                    trailing: Icon(
-                                      Icons.arrow_forward_ios,
-                                      color: colorBlack,
-                                    ),
-                                  ),
-                                ));
-                          });
-                    });
-              }),
-        ));
+                },
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
